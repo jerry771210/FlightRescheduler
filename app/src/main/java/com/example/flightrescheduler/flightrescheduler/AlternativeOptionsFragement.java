65ce181040c0a1378062b546c0d109d9fd2b4998
moumoutsay <weilints@andrew.cmu.edu>
2015-04-04 15:13:50 -0700

    update app/src/main/java/com/example/flightrescheduler/flightrescheduler/AlternativeOptionsFragement.java

--- app/src/main/java/com/example/flightrescheduler/flightrescheduler/AlternativeOptionsFragement.java
+++ app/src/main/java/com/example/flightrescheduler/flightrescheduler/AlternativeOptionsFragement.java
@@ -1,15 +1,16 @@
 package com.example.flightrescheduler.flightrescheduler;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,38 +18,52 @@ import java.util.List;
  */
 public class AlternativeOptionsFragement extends Fragment {
 
+    private final String[] mockOptions = {
+            "1 LAX - NYC Arrived at 08:00PM 10/23",
+            "2 LAX - NYC Arrived at 09:00PM 10/23",
+            "3 LAX - NYC Arrived at 05:00AM 10/24",
+            "4 LAX - NYC Arrived at 10:00AM 10/24",
+            "5 LAX - NYC Arrived at 11:00AM 10/24",
+            "6 LAX - NYC Arrived at 08:00PM 10/23",
+            "7 LAX - NYC Arrived at 09:00PM 10/23",
+            "8 LAX - NYC Arrived at 05:00AM 10/24",
+            "9 LAX - NYC Arrived at 10:00AM 10/24",
+            "10 LAX - NYC Arrived at 11:00AM 10/24",
+            "11 LAX - NYC Arrived at 08:00PM 10/23",
+            "12 LAX - NYC Arrived at 09:00PM 10/23",
+            "13 LAX - NYC Arrived at 05:00AM 10/24",
+            "14 LAX - NYC Arrived at 10:00AM 10/24",
+            "15 LAX - NYC Arrived at 11:00AM 10/24"
+    };
+
     public AlternativeOptionsFragement() {
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.alternatives_options, container, false);
-        List<String> weekForecast = new ArrayList<>();
-        weekForecast.add("1 LAX - NYC Arrived at 08:00PM 10/23");
-        weekForecast.add("2 LAX - NYC Arrived at 09:00PM 10/23");
-        weekForecast.add("3 LAX - NYC Arrived at 05:00AM 10/24");
-        weekForecast.add("4 LAX - NYC Arrived at 10:00AM 10/24");
-        weekForecast.add("5 LAX - NYC Arrived at 11:00AM 10/24");
 
-        weekForecast.add("6 LAX - NYC Arrived at 08:00PM 10/23");
-        weekForecast.add("7 LAX - NYC Arrived at 09:00PM 10/23");
-        weekForecast.add("8 LAX - NYC Arrived at 05:00AM 10/24");
-        weekForecast.add("9 LAX - NYC Arrived at 10:00AM 10/24");
-        weekForecast.add("10 LAX - NYC Arrived at 11:00AM 10/24");
-
-        weekForecast.add("11 LAX - NYC Arrived at 08:00PM 10/23");
-        weekForecast.add("12 LAX - NYC Arrived at 09:00PM 10/23");
-        weekForecast.add("13 LAX - NYC Arrived at 05:00AM 10/24");
-        weekForecast.add("14 LAX - NYC Arrived at 10:00AM 10/24");
-        weekForecast.add("15 LAX - NYC Arrived at 11:00AM 10/24");
-
-
+        /* For the list view part */
+        List<String> weekForecast = Arrays.asList(mockOptions);
         ArrayAdapter<String> weekForecastAdapter = new ArrayAdapter<String> (getActivity(), R.layout.list_item_available_route, R.id.list_item_available_route_textview, weekForecast);
-
         ListView lv = (ListView) rootView.findViewById(R.id.listviewAlternativeRoute);
         lv.setAdapter(weekForecastAdapter);
+
+        /* For the spinner part */
+        Spinner spinnerOptions = (Spinner) rootView.findViewById(R.id.spinnerAlternativeRoutes);
+        //Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_available_route, R.id.list_item_available_route_textview, mockOptions);
+        // Specify the layout to use when the list of choices appears
+//        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerOptions.setAdapter(spinnerAdapter);
 
         return rootView;
     }
