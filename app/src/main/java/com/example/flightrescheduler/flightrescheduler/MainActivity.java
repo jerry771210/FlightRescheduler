5aa0ba08216826d7c38307f1cf6e552da2ce2354
htliang <htliang@htliang.local>
2015-04-03 23:49:12 -0700

    Add simple login page

--- app/src/main/java/com/example/flightrescheduler/flightrescheduler/MainActivity.java
+++ app/src/main/java/com/example/flightrescheduler/flightrescheduler/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flightrescheduler.flightrescheduler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -25,6 +26,7 @@ public class MainActivity extends ActionBarActivity {
 //                    .add(R.id.container, new AlternativeOptionsFragement())
 //                    .commit();
         }
+        startActivity(new Intent(this, LoginActivity.class));
 
     }
 
