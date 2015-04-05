32e7297ee8b33c80828240e0df92a487cce73837
moumoutsay <weilints@andrew.cmu.edu>
2015-04-04 18:31:48 -0700

    tmp

--- app/src/main/java/edu/cmu/sv/flight/rescheduler/ui/AlternativeOptionsFragement.java
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/ui/AlternativeOptionsFragement.java
@@ -4,6 +4,7 @@ package edu.cmu.sv.flight.rescheduler.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,17 +20,18 @@ import java.util.List;
  * Created by Wei-Lin Tsai on 4/3/15.
  */
 public class AlternativeOptionsFragement extends Fragment {
+    static final String LOG_TAG = AlternativeOptionsFragement.class.getSimpleName();
 
     private final String[] mockOptions = {
-            "1 LAX - NYC Arrived at 08:00PM 10/23",
-            "2 LAX - NYC Arrived at 09:00PM 10/23",
-            "3 LAX - NYC Arrived at 05:00AM 10/24",
-            "4 LAX - NYC Arrived at 10:00AM 10/24",
-            "5 LAX - NYC Arrived at 11:00AM 10/24",
-            "6 LAX - NYC Arrived at 08:00PM 10/23",
-            "7 LAX - NYC Arrived at 09:00PM 10/23",
-            "8 LAX - NYC Arrived at 05:00AM 10/24",
-            "9 LAX - NYC Arrived at 10:00AM 10/24",
+            " 1 LAX - NYC Arrived at 08:00PM 10/23",
+            " 2 LAX - NYC Arrived at 09:00PM 10/23",
+            " 3 LAX - NYC Arrived at 05:00AM 10/24",
+            " 4 LAX - NYC Arrived at 10:00AM 10/24",
+            " 5 LAX - NYC Arrived at 11:00AM 10/24",
+            " 6 LAX - NYC Arrived at 08:00PM 10/23",
+            " 7 LAX - NYC Arrived at 09:00PM 10/23",
+            " 8 LAX - NYC Arrived at 05:00AM 10/24",
+            " 9 LAX - NYC Arrived at 10:00AM 10/24",
             "10 LAX - NYC Arrived at 11:00AM 10/24",
             "11 LAX - NYC Arrived at 08:00PM 10/23",
             "12 LAX - NYC Arrived at 09:00PM 10/23",
@@ -38,18 +40,36 @@ public class AlternativeOptionsFragement extends Fragment {
             "15 LAX - NYC Arrived at 11:00AM 10/24"
     };
 
+    private final String[] mockOtherAirlines = {
+            " 1 EVA LAX - NYC Arrived at 08:00PM 10/23",
+            " 2 EVA LAX - NYC Arrived at 09:00PM 10/23",
+            " 3 ANA LAX - NYC Arrived at 05:00AM 10/24",
+            " 4 ANA LAX - NYC Arrived at 10:00AM 10/24",
+            " 5 CI  LAX - NYC Arrived at 11:00AM 10/24",
+            " 6 CI  LAX - NYC Arrived at 08:00PM 10/23",
+            " 7 AA  LAX - NYC Arrived at 09:00PM 10/23",
+            " 8 AA  LAX - NYC Arrived at 05:00AM 10/24",
+            " 9 EVA LAX - NYC Arrived at 10:00AM 10/24",
+            "10 EVA LAX - NYC Arrived at 11:00AM 10/24",
+            "11 CI  LAX - NYC Arrived at 08:00PM 10/23",
+            "12 EVA LAX - NYC Arrived at 09:00PM 10/23",
+            "13 ANA LAX - NYC Arrived at 05:00AM 10/24",
+            "14 EVA LAX - NYC Arrived at 10:00AM 10/24",
+            "15 EVA LAX - NYC Arrived at 11:00AM 10/24"
+    };
+
     public AlternativeOptionsFragement() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_alternatives_options_basic_search, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_alternatives_options_basic_search, container, false);
 
         /* For the list view part */
-        List<String> weekForecast = Arrays.asList(mockOptions);
-        ArrayAdapter<String> weekForecastAdapter = new ArrayAdapter<String> (getActivity(), R.layout.list_item_available_route, R.id.list_item_available_route_textview, weekForecast);
+        List<String> alternativesList = Arrays.asList(mockOptions);
+        ArrayAdapter<String> alternativesAdapter = new ArrayAdapter<String> (getActivity(), R.layout.list_item_available_route, R.id.list_item_available_route_textview, alternativesList);
         ListView lv = (ListView) rootView.findViewById(R.id.listviewAlternativeRoute);
-        lv.setAdapter(weekForecastAdapter);
+        lv.setAdapter(alternativesAdapter);
 
         /* For the spinner part */
         Spinner spinnerOptions = (Spinner) rootView.findViewById(R.id.spinnerAlternativeRoutes);
@@ -79,6 +99,50 @@ public class AlternativeOptionsFragement extends Fragment {
             }
         });
 
+        buttonOtherAirlines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Click other airlines");
+                /* Update view part */
+                List<String> otherAirlinesList= Arrays.asList(mockOtherAirlines);
+                ArrayAdapter<String> otherLinesAdapter = new ArrayAdapter<String> (getActivity(), R.layout.list_item_available_route, R.id.list_item_available_route_textview, otherAirlinesList);
+                ListView lv = (ListView) rootView.findViewById(R.id.listviewAlternativeRoute);
+                lv.setAdapter(otherLinesAdapter);
+                updateSwitchAirLinesButtonVal(v);
+            }
+        });
+
         return rootView;
     }
+
+    private void updateSwitchAirlinesListView(View v) {
+        // get the button current value
+        Button buttonOtherAirlines = (Button) v.findViewById(R.id.buttonOtherAirlines);
+        String buttonVal = buttonOtherAirlines.getText().toString();
+        if ("Other Airlines".equals(buttonVal)) {
+            // change to other airlines
+        } else {
+            // change to
+        }
+
+        return;
+    }
+
+    private void updateSwitchAirLinesSpinner(View v) {
+        return;
+    }
+
+    private void updateSwitchAirLinesButtonVal(View v) {
+        // get the button current value
+        Button buttonOtherAirlines = (Button) v.findViewById(R.id.buttonOtherAirlines);
+        String buttonVal = buttonOtherAirlines.getText().toString();
+        if ("Other Airlines".equals(buttonVal)) {
+            buttonOtherAirlines.setText("Original Airlines");
+        } else {
+            buttonOtherAirlines.setText("Other Airlines");
+        }
+        return;
+    }
+
+
 }
