baf821feb769bfe068beadd6004cf0ba5f0085f9
moumoutsay <weilints@andrew.cmu.edu>
2015-04-10 17:19:20 -0700

    add empty file to create directory for each package

---
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/model/EmptyModel.java
@@ -0,0 +1,7 @@
+package edu.cmu.sv.flight.rescheduler.model;
+
+/**
+ * Created by moumoutsay on 4/10/15.
+ */
+public class EmptyModel {
+}
---
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/util/db/EmptyUtil.java
@@ -0,0 +1,7 @@
+package edu.cmu.sv.flight.rescheduler.util.db;
+
+/**
+ * Created by moumoutsay on 4/10/15.
+ */
+public class EmptyUtil {
+}
