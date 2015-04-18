b8cc2b7d5b2c4742526d5ec733a56e74b1fb7f06
moumoutsay <weilints@andrew.cmu.edu>
2015-04-18 16:31:16 -0700

    Refactor: change naming

--- app/src/main/java/edu/cmu/sv/flight/rescheduler/database/sql/SQLCmdAirportRoute.java
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/database/sql/SQLCmdAirportRoute.java
@@ -5,7 +5,7 @@ import android.provider.BaseColumns;
 /**
  * Created by hsuantzl on 2015/4/18.
  */
-public abstract class AirportRoute implements BaseColumns {
+public abstract class SQLCmdAirportRoute implements BaseColumns {
     public static final String TABLE_NAME = "airport_route";
     public static final String FROM_AIRPORT_ID = "from_airport_id";
     public static final String TO_AIRPORT_ID = "to_airport_id";
@@ -15,9 +15,9 @@ public abstract class AirportRoute implements BaseColumns {
             FROM_AIRPORT_ID + " INTEGER," +
             TO_AIRPORT_ID + " INTEGER," +
             " FOREIGN KEY(" + FROM_AIRPORT_ID + ")" +
-            " REFERENCES " + Airport.TABLE_NAME + "(" + Airport._ID + ")" +
+            " REFERENCES " + SQLCmdAirport.TABLE_NAME + "(" + SQLCmdAirport._ID + ")" +
             " ON DELETE CASCADE ON UPDATE CASCADE, " +
             " FOREIGN KEY(" + TO_AIRPORT_ID + ")" +
-            " REFERENCES " + Airport.TABLE_NAME + "(" + Airport._ID + ")" +
+            " REFERENCES " + SQLCmdAirport.TABLE_NAME + "(" + SQLCmdAirport._ID + ")" +
             " ON DELETE CASCADE ON UPDATE CASCADE );";
 }
