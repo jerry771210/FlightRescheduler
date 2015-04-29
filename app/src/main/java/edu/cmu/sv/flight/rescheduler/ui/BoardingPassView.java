357a26ecc54d7bd540752c30a754309817f609a7
moumoutsay <weilints@andrew.cmu.edu>
2015-04-28 20:44:33 -0700

    Revert "boarding pass view update for demo."
    This reverts commit 4cef3897b48f7cf29fc377e48adaa4d2415b03dd.

--- app/src/main/java/edu/cmu/sv/flight/rescheduler/ui/BoardingPassView.java
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/ui/BoardingPassView.java
@@ -19,11 +19,6 @@ public class BoardingPassView {
     private TextView textViewStatus;
     private RelativeLayout layoutTop;
     private BoardingPass boardingPass;
-    private TextView textViewDepart;
-    private TextView textViewArrive;
-    private TextView textViewFlightNum;
-    private TextView textViewTerminal;
-    private TextView textViewGate;
 
     public BoardingPassView(Activity activity, LayoutInflater inflater, BoardingPass boardingPass) {
         this.activity = activity;
@@ -31,12 +26,6 @@ public class BoardingPassView {
         this.boardingPass = boardingPass;
 
         textViewStatus = (TextView) view.findViewById(R.id.textViewStatus);
-        textViewDepart = (TextView) view.findViewById(R.id.textViewDeparture);
-        textViewArrive = (TextView) view.findViewById(R.id.textViewArrival);
-        textViewFlightNum = (TextView) view.findViewById(R.id.textViewFlightNumber);
-        textViewTerminal = (TextView) view.findViewById(R.id.textViewTerminal);
-        textViewGate = (TextView) view.findViewById(R.id.textViewGate);
-
         layoutTop = (RelativeLayout) view.findViewById(R.id.RelativeLayoutTop);
         configure();
     }
@@ -50,40 +39,20 @@ public class BoardingPassView {
             case LANDED:  // Default layout is landed
                 textViewStatus.setText("Landed");
                 layoutTop.setBackgroundColor(0xC8003269);
-                textViewDepart.setText("TPE");
-                textViewArrive.setText("NRT");
-                textViewFlightNum.setText("824");
-                textViewTerminal.setText("T5");
-                textViewGate.setText("G08");
                 break;
             case ON_TIME:
                 textViewStatus.setText("On Time");
                 layoutTop.setBackgroundColor(0xC8176935);
-                textViewDepart.setText("NRT");
-                textViewArrive.setText("LAX");
-                textViewFlightNum.setText("6");
-                textViewTerminal.setText("T3");
-                textViewGate.setText("G18");
                 break;
             case DELAYED:
                 textViewStatus.setText("Delayed");
                 layoutTop.setBackgroundColor(0xC8EBB027);
                 view.setOnClickListener(new IntentToActivityOnClickListener(activity, AlternativeOptionsActivity.class));
-                textViewDepart.setText("LAX");
-                textViewArrive.setText("NYC");
-                textViewFlightNum.setText("667");
-                textViewTerminal.setText("T6");
-                textViewGate.setText("G11");
                 break;
             case CANCELED:
                 textViewStatus.setText("Canceled");
                 layoutTop.setBackgroundColor(0xC8EB0300);
                 view.setOnClickListener(new IntentToActivityOnClickListener(activity, AlternativeOptionsActivity.class));
-                textViewDepart.setText("NYC");
-                textViewArrive.setText("BOS");
-                textViewFlightNum.setText("2180");
-                textViewTerminal.setText("T8");
-                textViewGate.setText("G21");
                 break;
         }
     }
