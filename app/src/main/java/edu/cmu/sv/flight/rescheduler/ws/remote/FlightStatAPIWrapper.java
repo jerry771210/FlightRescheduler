4a5681e9a32dcc6e58328390f2825fc604915316
moumoutsay <weilints@andrew.cmu.edu>
2015-04-30 23:07:49 -0700

    tmp version of FlightStatXXX

--- app/src/main/java/edu/cmu/sv/flight/rescheduler/ws/remote/FlightStatAPIWrapper.java
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/ws/remote/FlightStatAPIWrapper.java
@@ -1,12 +1,51 @@
 package edu.cmu.sv.flight.rescheduler.ws.remote;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
 /**
  * Created by moumoutsay on 4/17/15.
  */
 public class FlightStatAPIWrapper implements IPublicFlightAPI {
     public String getFlight(String fromAirport, String toAirport, Date date) {
+        String rawJSON = "";
         // Do the real REST query here
+        FlightStatQueryURL flightStatQueryURL = new FlightStatQueryURL();
+        String strURL = flightStatQueryURL.makeQueryURL(fromAirport, toAirport, date);
+        boolean query_fail = false;
+
+        // get raw data
+        try {
+            String tmpString;
+            URL targetURL = new URL(strURL);
+            URLConnection uc = targetURL.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+            while ((tmpString = in.readLine()) != null) {
+                rawJSON = rawJSON + tmpString;
+            }
+            in.close();
+        } catch (Exception e) {
+            query_fail = true;
+            e.printStackTrace();
+        }
+
+//        // get flights
+//        try {
+//            JSONObject j_obj_all = new JSONObject(rawJSON);
+//            json_arr_all_dep_flights = j_obj_all.getJSONArray("scheduledFlights");
+//            //System.out.println("The numbers of flights" + json_arr_all_dep_flights.length());
+//        } catch (Exception e){
+//            query_fail = true;
+//            e.printStackTrace();
+//        }
+//
+//        return query_fail;
+
+
         return null;
-    }   
+    }
+
+
 }
---
+++ app/src/main/java/edu/cmu/sv/flight/rescheduler/ws/remote/FlightStatQueryURL.java
@@ -0,0 +1,37 @@
+package edu.cmu.sv.flight.rescheduler.ws.remote;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by moumoutsay on 4/30/15.
+ */
+public class FlightStatQueryURL {
+    private String d_a; // departure airport
+    private String r_a; // return airport
+    private int d_d; // departure date
+    private int d_m; // departure month
+    private int d_y; // departure year
+
+    public String makeQueryURL(String fromAirport, String toAirport, Date date){
+        d_a = fromAirport;
+        r_a = toAirport;
+        parse_date(date);
+
+        String strURL = "https://api.flightstats.com/flex/schedules/rest/v1/json/"
+                + "from/" + d_a
+                + "/to/" + r_a
+                + "/departing/"
+                + d_y + "/" + d_m + "/" + d_d
+                + "/?";   // KEY and ID should not upload
+        return strURL;
+    }
+
+    private void parse_date(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        d_y = cal.get(Calendar.YEAR);
+        d_m = cal.get(Calendar.MONTH) + 1; // Month start from 0
+        d_d = cal.get(Calendar.DAY_OF_MONTH);
+    }
+}
